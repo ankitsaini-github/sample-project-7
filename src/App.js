914f16ef0400a97dfccf679803c0c9e0dcfa8d22
ankitsaini-github <ankit121saini@gmail.com>
2024-01-25 16:36:13 +0530

    converted to async await

--- src/App.js
+++ src/App.js
@@ -5,22 +5,42 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
-  function fetchMoviesHandler() {
-    fetch('https://swapi.dev/api/films/')
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformedMovies);
-      });
+  // function fetchMoviesHandler() {
+  //   fetch('https://swapi.dev/api/films/')
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       const transformedMovies = data.results.map((movieData) => {
+  //         return {
+  //           id: movieData.episode_id,
+  //           title: movieData.title,
+  //           openingText: movieData.opening_crawl,
+  //           releaseDate: movieData.release_date,
+  //         };
+  //       });
+  //       setMovies(transformedMovies);
+  //     });
+  // }
+  async function fetchMoviesHandler() {
+    try {
+      console.log('fetching data...')
+      const response = await fetch('https://swapi.dev/api/films/');
+      console.log('response recieved, converting to data...')
+      const data = await response.json();
+      console.log('data recieved, mapping...')
+
+      const transformedMovies = data.results.map((movieData) => ({
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      }));
+
+      setMovies(transformedMovies);
+    } catch (error) {
+      console.error('Error fetching movies:', error);
+    }
   }
   return (
     <React.Fragment>
